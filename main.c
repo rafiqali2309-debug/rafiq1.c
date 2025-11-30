dc2632013486e747c31b7997d7cca4775dd121cb
rafiqali2309-debug <rafiqali2309@gmail.com>
2025-11-30 03:58:56 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float length, breadth, area, perimeter;
+    printf("length of the rectangle: ");
+    scanf("%f", &length);
+    printf("breadth of the rectangle:");
+    scanf("%f", &breadth);
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+    printf("area of the plot is: %.2f\n", area);
+    printf("perimeter of the plot: %.2f\n", perimeter);
+
+    return 0;
+
+}
